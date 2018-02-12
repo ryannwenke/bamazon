24a7ade00e12a07ff61fa16357afd9cf29a76abb
Ryann Wenke <ryannwenke@gmail.com>
2018-02-11 22:27:22 -0700

    second commit

---
+++ bamazon.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE bamazon;
+USE bamazon;
---
+++ bamazonCustomer.js
@@ -0,0 +1,12 @@
+//Display all items for sale
+
+//Prompt users with two messages
+//First ask item ID
+//Then ask how many units they would like to purchase
+
+//Once customer places order, check if store has enough to meet their demands
+//If not, app should say Insufficient quantity! and prevent order from going through
+
+//If the store does have enough, order should go through
+	//Update SQL database to reflect remaining quantity
+	//Once update goes through, show customer total cost of their order
