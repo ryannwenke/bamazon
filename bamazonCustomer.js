92f03d5abd3bdb5058c605a32380506a8f3ee763
Ryann Wenke <ryannwenke@gmail.com>
2018-02-15 22:04:22 -0700

    connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,23 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const table = require("cli-table");
+const colors = require("colors");
 
-// let connection = mysql.createConnection({
-// 	host: "localhost",
-// 	port: 3000,
-// 	username: "root",
-// 	password:"",
-// 	database: "bamazon_db"
-// });
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3000,
+	user: "root",
+	password: "Ridicul0us!",
+	database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log("connected as id " + connection.threadId);
+	printItems(function() {
+		selectItem();
+	});
+});
 
 //Display all items for sale
 
